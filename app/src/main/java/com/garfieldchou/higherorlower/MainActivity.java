cd7ab7e2156095f1541d24c3bd4a4909ccdc5136
shaudai <shaudai0402@hotmail.com>
2016-12-14 22:56:00 +0800

    Commit for layout and onclick template

--- app/src/main/java/com/garfieldchou/higherorlower/MainActivity.java
+++ app/src/main/java/com/garfieldchou/higherorlower/MainActivity.java
@@ -2,8 +2,14 @@ package com.garfieldchou.higherorlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void numGuess (View view) {
+        Log.i("Info", "Guess button was clicked !");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
