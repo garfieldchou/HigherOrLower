faefc0221f0e1865b37853bd908c62ec7309d6b8
shaudai <shaudai0402@hotmail.com>
2016-12-15 07:29:35 +0800

    Get a random number for guess

--- app/src/main/java/com/garfieldchou/higherorlower/MainActivity.java
+++ app/src/main/java/com/garfieldchou/higherorlower/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Info", "Guess button was clicked !");
         EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
         Log.i("Info", "guess number: " + guessEditText.getText().toString());
+        Random r = new Random();
+        int numToGuess = r.nextInt(20 - 1) + 1 ;
+        Log.i("Info", "number to guess: " + numToGuess) ;
     }
 
     @Override
