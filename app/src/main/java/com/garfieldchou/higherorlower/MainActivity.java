62eef8f8e1c220786c10ce2261736b825ea9a0b6
shaudai <shaudai0402@hotmail.com>
2016-12-15 07:22:26 +0800

    Get the EditText content for the guess number

--- app/src/main/java/com/garfieldchou/higherorlower/MainActivity.java
+++ app/src/main/java/com/garfieldchou/higherorlower/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public void numGuess (View view) {
         Log.i("Info", "Guess button was clicked !");
+        EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
+        Log.i("Info", "guess number: " + guessEditText.getText().toString());
     }
 
     @Override
